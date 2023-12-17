01b81374f6c0cc5d2b46993f8a432d44936bb379
榊原昌彦 <sakakibara@rdlabo.jp>
2023-12-17 20:50:32 +0900

    feat(): prepare release flow

---
+++ release.mjs
@@ -0,0 +1,21 @@
+import { updatePackage } from 'write-pkg';
+import path from 'path';
+import pkg from './package.json' assert { type: 'json' };
+import { exec } from 'child_process';
+
+const workspaces = ['cli'];
+
+workspaces.forEach(async (workspace) => {
+  const buildPackagePath = path.resolve('./packages/' + workspace);
+  await updatePackage(buildPackagePath + '/package.json', { version: pkg.version });
+  exec(
+    `cd ${buildPackagePath} && npm publish --access=public`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    },
+  );
+});
