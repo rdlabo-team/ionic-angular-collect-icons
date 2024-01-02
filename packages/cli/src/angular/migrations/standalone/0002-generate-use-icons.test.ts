73c3303abca71b543a1eb2995906967deb3f363d
榊原昌彦 <sakakibara@rdlabo.jp>
2024-01-02 11:39:27 +0900

    feat(): support new control flow with template inner @Component

--- packages/cli/src/angular/migrations/standalone/0002-generate-use-icons.test.ts
+++ packages/cli/src/angular/migrations/standalone/0002-generate-use-icons.test.ts
@@ -38,4 +38,37 @@ describe("migrateComponents", () => {
       );
     });
   });
+
+  describe("standalone angular components with control flow", () => {
+    it("should detect and import icons used in the template", async () => {
+      const project = new Project({ useInMemoryFileSystem: true });
+
+      const component = `
+        import { Component } from "@angular/core";
+
+        @Component({
+          selector: 'my-component',
+          template: '@if(1 === 1) {<ion-icon name="logo-ionic"></ion-icon><ion-icon name="close-outline"></ion-icon> }',
+          standalone: true
+        }) 
+        export class MyComponent { }
+      `;
+
+      project.createSourceFile("foo.component.ts", dedent(component));
+
+      const useIconFile = project.createSourceFile("use-icons.ts", dedent(``));
+
+      await generateUseIcons(project, {
+        dryRun: false,
+        iconPath: "src/use-icons.ts",
+        projectPath: cwd(),
+        interactive: false,
+        initialize: false,
+      });
+
+      expect(dedent(useIconFile.getText())).toBe(
+        dedent(`export { logoIonic, closeOutline } from "ionicons/icons";`),
+      );
+    });
+  });
 });
